edd0ed9ae9a3c3657b37e2bc99ad0a835004368d
NeroAlex9 <Nerobelov1997@mail.ru>
2024-10-01 14:27:23 +0300

    update js

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
+let currentIndex = 0;
+
+    function showSlide(index) {
+        const slides = document.querySelector('.slides');
+        const dots = document.querySelectorAll('.dot');
+
+        if (index >= slides.children.length) {
+            currentIndex = 0;
+        } else if (index < 0) {
+            currentIndex = slides.children.length - 1;
+        } else {
+            currentIndex = index;
+        }
+
+        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+        
+        dots.forEach((dot, i) => {
+            dot.classList.toggle('active-dot', i === currentIndex);
+        });
+    }
+
+    function currentSlide(index) {
+        showSlide(index);
+    }
+
+    // Автоматическое переключение слайдов каждые 7 секунд
+    setInterval(() => {
+        showSlide(currentIndex + 1);
+    }, 7000);
+
+    // Изначально отображаем первый слайд
+    showSlide(currentIndex);
+
+document.querySelector('.green-btn').addEventListener('click', function() {
+    document.querySelector('#join_us_id').scrollIntoView({
+        behavior: 'smooth' 
+    });
+});
+
+
 const feedbackItems = document.querySelectorAll('.feedback-item');
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
-let currentIndex = 0;
+let currentIndex1 = 0;
 const itemsPerPage = 3; // Количество отображаемых отзывов
 
 function updateFeedbackDisplay() {
     feedbackItems.forEach((item, index) => {
         item.classList.remove('active');
-        if (index >= currentIndex && index < currentIndex + itemsPerPage) {
+        if (index >= currentIndex1 && index < currentIndex1 + itemsPerPage) {
             item.classList.add('active');
         }
     });
     
     // Управление видимостью кнопок
-    prevBtn.style.display = currentIndex > 0 ? 'block' : 'none';
-    nextBtn.style.display = currentIndex < feedbackItems.length - itemsPerPage ? 'block' : 'none';
+    prevBtn.style.display = currentIndex1 > 0 ? 'block' : 'none';
+    nextBtn.style.display = currentIndex1 < feedbackItems.length - itemsPerPage ? 'block' : 'none';
 }
 
 function showNext() {
-    if (currentIndex < feedbackItems.length - itemsPerPage) {
-        currentIndex++;
+    if (currentIndex1 < feedbackItems.length - itemsPerPage) {
+        currentIndex1++;
     }
     updateFeedbackDisplay();
 }
 
 function showPrev() {
-    if (currentIndex > 0) {
-        currentIndex--;
+    if (currentIndex1 > 0) {
+        currentIndex1--;
     }
     updateFeedbackDisplay();
 }
@@ -35,3 +75,7 @@ prevBtn.addEventListener('click', showPrev);
 nextBtn.addEventListener('click', showNext);
 
 updateFeedbackDisplay(); // Инициализируем отображение
+
+
+
+
